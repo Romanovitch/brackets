2f567a71f80c5d0fd563316611d0503e7c70083a
Romanovitch <rokos.job.ru@gmail.com>
2019-10-08 12:36:49 +0300

    feat: implement check()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  function oneReplacer(str, myConf){
+    myConf.forEach(iBrts => str = str.replace(iBrts, ""));
+    myConf.forEach(iBrts => (str.includes(iBrts) 
+      ? str = oneReplacer(str, myConf) 
+      : str))
+    return str;
+  }    
+  var myConf = bracketsConfig.map(iArr => iArr.join(""));
+  return !oneReplacer(str, myConf).length ? true : false;
 }
